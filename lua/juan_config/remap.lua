3ffcd29f94bb7656a7cf9f94f671fce759f2f0d1
Juan Antonio Barragan <jbarrag3@jh.edu>
2024-12-21 13:58:24 -0500

    NNN remaps

--- lua/juan_config/remap.lua
+++ lua/juan_config/remap.lua
@@ -2,3 +2,8 @@ vim.keymap.set("i", "jk", "<Esc>")
 
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+
+-- nnn.nvim
+vim.keymap.set("n", "<leader>n", ":NnnPicker <CR>")
+vim.keymap.set("n", "<leader>t", ":NnnExplorer <CR>")
