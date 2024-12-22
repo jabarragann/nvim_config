83b67a6ee28954df5b353b59678e1902fae1659a
Juan Antonio Barragan <jbarrag3@jh.edu>
2024-12-22 02:15:58 -0500

    splits remaps and nnn config

--- lua/juan_config/remap.lua
+++ lua/juan_config/remap.lua
@@ -3,7 +3,13 @@ vim.keymap.set("i", "jk", "<Esc>")
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Splits remaps
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
 
 -- nnn.nvim
 vim.keymap.set("n", "<leader>n", ":NnnPicker <CR>")
 vim.keymap.set("n", "<leader>t", ":NnnExplorer <CR>")
+
