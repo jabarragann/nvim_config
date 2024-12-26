b6d5ba8da732eeb83ede4a06c5e4a204bf3e0146
Juan Antonio Barragan <jbarrag3@jh.edu>
2024-12-26 01:16:05 -0500

    Config tabs in editor

--- lua/juan_config/editor.lua
+++ lua/juan_config/editor.lua
@@ -1,3 +1,10 @@
 -- Splits configurations
 vim.opt.splitbelow = true
 vim.opt.splitright = true
+
+
+vim.opt_local.tabstop = 4
+vim.opt_local.shiftwidth = 4
+vim.opt_local.expandtab = true
+vim.opt_local.softtabstop = 4
+vim.opt_local.number= true
