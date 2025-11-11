9e2658ad4b7e9cc795b80a97bef77da2bea96c7d
Juan Antonio Barragan <jbarrag3@jh.edu>
2025-11-11 13:38:43 -0500

    Change plugin version file if in Ubuntu 20.04

--- lua/juan_config/lazy.lua
+++ lua/juan_config/lazy.lua
@@ -17,12 +17,28 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 -- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
+-- loading lazy.nvim so that mappings are correct. (done in remaps.lua)
 -- This is also a good place to setup other settings (vim.opt)
 
--- DONE in remaps.lua
--- vim.g.mapleader = " "
--- vim.g.maplocalleader = "\\"
+-- Load different version of the plugins depending on the ubuntu version
+local function get_lockfile_path()
+  local default = vim.fn.stdpath("config") .. "/lazy-lock.json"
+  local uname = vim.loop.os_uname()
+
+  if uname.sysname ~= "Linux" then
+    return default
+  end
+
+  local handle = io.popen("lsb_release -rs 2>/dev/null")
+  local version = handle and handle:read("*l") or ""
+  if handle then handle:close() end
+
+  if version:match("^20%.04") then
+    return vim.fn.stdpath("config") .. "/lazy-lock-frozen/lazy-lock-nvim0.10-ubuntu20.04_test.json"
+  end
+
+  return default
+end
 
 -- Setup lazy.nvim
 require("lazy").setup({
@@ -35,4 +51,34 @@ require("lazy").setup({
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
+  -- Ensure lockfile exists or else a new one will be created.
+  lockfile = get_lockfile_path(), 
+  -- lockfile = vim.fn.stdpath("config") .. "/lazy-lock-frozen/lazy-lock-nvim0.10-ubuntu20.04_test.json"
 })
+
+
+-- Optional helper commands for quick switching
+-- Also for testing which file is loaded
+-- :lua print(require("lazy.core.config").options.lockfile)
+
+
+-- FAILED EXPERIMENT:
+-- It seems like at setup the file selected is loaded and
+-- then changing the file will result on overwritting
+-- the different lock-files
+--
+-- vim.api.nvim_create_user_command("LazyUseLegacy", function()
+--   local cfg = require("lazy.core.config")
+--   local lockfile = vim.fn.stdpath("config")
+--     .. "/lazy-lock-frozen/"
+--     .. "lazy-lock-nvim0.10-ubuntu20.04_test.json"
+--   cfg.options.lockfile = lockfile
+--   vim.cmd("Lazy restore")
+-- end, {})
+--
+-- vim.api.nvim_create_user_command("LazyUseCurrent", function()
+--   local cfg = require("lazy.core.config")
+--   local lockfile = vim.fn.stdpath("config") .. "/lazy-lock.json"
+--   cfg.options.lockfile = lockfile
+--   vim.cmd("Lazy restore")
+-- end, {})
