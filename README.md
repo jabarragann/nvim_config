27b4815f5b4764154f296dfd81f0e30719b7e690
Juan Antonio Barragan <jbarrag3@jh.edu>
2026-03-04 23:06:34 -0500

    Plugin update

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 # Juan NVIM config
 
+## Latest setup
+
+* Kitty version:
+* NVIM version:
+* Operating systems:
+    * Ubuntu 24.04
+
+## Plugin notes:
+
+* Kitty-scrollback.nvim: 
+    * Version 8.0.0: Remove support for Kitty <0.43.0
+    * Version 7.0.0: Remove support of Nvim <0.10.0
+
+## Additional tests
 * Tested with NVIM 0.11.5 in Ubuntu 24.04
 * Tested with NVIM 0.11.5 in Ubuntu 22.04
--- lua/plugins/kitty-scrollback.lua
+++ lua/plugins/kitty-scrollback.lua
@@ -5,7 +5,8 @@ return {
     cmd = { 'KittyScrollbackGenerateKittens', 'KittyScrollbackCheckHealth', 'KittyScrollbackGenerateCommandLineEditing' },
     event = { 'User KittyScrollbackLaunch' },
     -- version = '*', -- latest stable version, may have breaking changes if major version changed
-    version = '^6.2.0', -- pin major version, include fixes and features that do not have breaking changes
+    version = '^9.2.0', -- pin major version, include fixes and features that do not have breaking changes
+    -- kitty <0.43.0 requires 7.0.0
     config = function()
       require('kitty-scrollback').setup()
     end,
