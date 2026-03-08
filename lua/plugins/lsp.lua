72605598f0c3a38c8d994caba9e42ef986400eb1
Juan Antonio Barragan <jbarrag3@jh.edu>
2026-03-08 01:59:03 -0500

    Add LSP signature plugin

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,7 +8,18 @@ return {
                 "MunifTanjim/nui.nvim"
             },
         }},
+    -- LSP completion
     {'hrsh7th/cmp-nvim-lsp'},
-    {'hrsh7th/nvim-cmp'}
+    {'hrsh7th/nvim-cmp'},
+    {
+      "ray-x/lsp_signature.nvim",
+        event = "InsertEnter",
+        opts = {
+          bind = true,
+          handler_opts = {
+            border = "rounded",
+          },
+        },
+      },
 }
 
