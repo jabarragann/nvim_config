ea6c38deda3e791a8a4c22ddcc4ad87fc53791ec
Juan Antonio Barragan <jbarrag3@jh.edu>
2026-03-09 20:38:47 -0400

    Fix keymap collision

--- after/plugin/fzf-lua.lua
+++ after/plugin/fzf-lua.lua
@@ -1,5 +1,7 @@
 local builtin = require("fzf-lua")
 
+-- Improve this keymaps.
+-- check if the there is a git project and if there is use git_files instead of files for <C-P>.
 vim.keymap.set('n', '<C-p>', builtin.files, { desc = "Fzf Files" }) 
 vim.keymap.set('n', '<leader>g', builtin.git_files, { desc = "FZF git Files" }) 
 vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = "FZF buffer Files" }) 
--- after/plugin/git_tools.lua
+++ after/plugin/git_tools.lua
@@ -1,6 +1,7 @@
 gitsigns = require("gitsigns")
 
-vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk_inline<CR>", { desc = "Preview git hunk" })
+-- Collides with my other keymaps for fzflua gitfiles --> <leader>p
+-- vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk_inline<CR>", { desc = "Preview git hunk" })
 
 
 
