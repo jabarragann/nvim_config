731f57dcb3dc58743621b8e870ee79b5ba0d11ba
Juan Antonio Barragan <jbarrag3@jh.edu>
2026-03-10 18:49:11 -0400

    Update fzf-lua keymap to look for files

--- after/plugin/fzf-lua.lua
+++ after/plugin/fzf-lua.lua
@@ -1,7 +1,19 @@
 local builtin = require("fzf-lua")
 
+local function project_files()
+  vim.fn.system("git rev-parse --is-inside-work-tree 2>/dev/null")
+  if vim.v.shell_error == 0 then
+    builtin.git_files()
+  else
+    builtin.files()
+  end
+end
+
+vim.keymap.set("n", "<C-p>", project_files, { desc = "Find project files" })
+
 -- Improve this keymaps.
 -- check if the there is a git project and if there is use git_files instead of files for <C-P>.
-vim.keymap.set('n', '<C-p>', builtin.files, { desc = "Fzf Files" }) 
+-- vim.keymap.set('n', '<C-p>', builtin.files, { desc = "Fzf Files" }) 
+
 vim.keymap.set('n', '<leader>g', builtin.git_files, { desc = "FZF git Files" }) 
 vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = "FZF buffer Files" }) 
