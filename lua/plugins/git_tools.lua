09f4ffa4ff51df98ec3595b2e89164f5bdfa3a03
Juan Antonio Barragan <jbarrag3@jh.edu>
2026-07-19 12:50:42 -0400

    Add git unimpaired

--- lua/plugins/git_tools.lua
+++ lua/plugins/git_tools.lua
@@ -2,6 +2,8 @@ return {
     { "tpope/vim-fugitive"},
     -- Enables :GBrowse -- See: https://github.com/tpope/vim-fugitive/issues/2309
     {"tpope/vim-rhubarb"},
+    -- Nav shortcuts --
+    {"tpope/vim-unimpaired"},
     {
         "lewis6991/gitsigns.nvim",
         config = function()
