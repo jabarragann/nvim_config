39bccc747e02fd3be6d31c7e9b6f8d45685b84ad
Juan Antonio Barragan <jbarrag3@jh.edu>
2026-07-22 18:09:08 -0400

    "Add vim-table-mode to plugins"

---
+++ lua/plugins/vim-table-mode.lua
@@ -0,0 +1,7 @@
+return {
+    {
+        "dhruvasagar/vim-table-mode",
+        -- Load plugin only when one the following commands is executed 
+        cmd = { "TableModeToggle", "TableModeEnable", "TableModeRealign", "Tableize" },
+    }
+}
